0284d7dc1b39f0d5820f43cdecda9a6e9d830e2a
Jeremy Anderson <jande081@fiu.edu>
2017-10-25 16:34:39 -0400

    First Commit, ready for lesson

---
+++ practice .js
@@ -0,0 +1,28 @@
+var count;
+
+while(count > 0){
+
+}
+
+
+//Exercise 1
+//Print out in the console
+//
+//I ran...
+//  I ran...
+//      I ran...
+//Then I got tired and stopped
+//
+//Make sure you use if statements inside to help you out.
+//Also remember that the last line will be printed out first
+
+
+//Exercise 2
+//Print out the first 20 multiples of 9 but the counter can be only incremented by 1
+//i.e. count-- or count++
+
+//Exercise3
+//In a month a bank account is acrewing intrest of 2% a month. 
+//Using a variable called bank account that has an initial value of your choosing.
+//Use a while loop to print out the value of that account every month for the next 24 months.
+//The display must be in dollar form. For example, $216.34
